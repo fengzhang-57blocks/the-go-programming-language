7343af1b7ae384b74aa91b1318caa1c230de01b3
mkjfeng01 <592676493@qq.com>
2022-07-14 21:31:31 +0800

    ch5

---
+++ ch5/squares/squares.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// squares函数返回一个函数，后者包含下一次要用到的平方数
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
